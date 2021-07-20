d7a6b45efeccdd9f0cd55637c6adca4c6df479bf
peppe mursia <giuseppe.mursia@protom.com>
2021-07-20 19:28:55 +0200

    update exercise

---
+++ typescript/exercise/exercise5.js
@@ -0,0 +1,26 @@
+
+
+
+function myFunction() {
+    let text;
+    let stringa = prompt();
+    if ( stringa.length > 100 || !stringa.match(/[a-z]/i)) {
+      text = "La stringa inserita non è valida" ;
+    } else {
+        let charachter = prompt("Quale carattere vuoi contare");
+      if(charachter.length != 1){
+        text= "il carattere inserito non è valido"
+    }else{
+            let counts = 0;
+            for(let i in stringa)
+            charachter.includes(stringa[i])?counts++:false;
+
+        text = 'Il carattere inserito ' + charachter +'  si ripete:' +  '\n' + counts+' volte' 
+    }
+
+} 
+document.getElementById("demo").innerHTML = text;
+    }
+
+  
+      
---
+++ typescript/exercise/exercise5.ts
@@ -0,0 +1,23 @@
+
+function myFunction() {
+    let text : string;
+    let stringa: any = prompt();
+    if ( stringa.length > 100 || !stringa.match(/[a-z]/i)) {
+      text = "La stringa inserita non è valida" ;
+    } else {
+        let charachter: string = prompt("Quale carattere vuoi contare");
+      if(charachter.length != 1){
+        text= "il carattere inserito non è valido"
+    }else{
+            let counts: number = 0;
+            for(let i in stringa)
+            charachter.includes(stringa[i])?counts++:false;
+
+        text = 'Il carattere inserito ' + charachter +'  si ripete:' +  '\n' + counts+' volte' 
+    }
+
+} 
+document.getElementById("demo").innerHTML = text;
+    }
+
+  
